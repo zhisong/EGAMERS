d99dc4e82b9ce62d132af599c71dea703a0e5fea
Zhisong Qu <zhisong.qu@gmail.com>
2022-03-17 13:51:49 +1100

    compute growth rate

--- py/pyegamers/egamers.py
+++ py/pyegamers/egamers.py
@@ -1,36 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class EGAMERS:
 
-    from .ncfield import read_ncfield, compute_ncfield, plot_field_radial, plot_field_time, plot_spectrogram
+    from .ncfield import (
+        read_ncfield,
+        compute_ncfield,
+        compute_gamma,
+        plot_field_radial,
+        plot_field_time,
+        plot_spectrogram,
+        plot_field_energy
+    )
 
     class OutputData:
         pass
 
-    def __init__(self, folder_path='./'):
-        """! Read data from given path
-        """
+    def __init__(self, folder_path="./"):
+        """! Read data from given path"""
         import os
+
         abspath = os.path.abspath(folder_path)
 
-        ncfield_name = os.path.join(abspath, 'snapshot.field.nc')
+        namelist_name = os.path.join(abspath, "namelist.in")
+        if os.path.exists(namelist_name):
+            import f90nml
+            self.namelist = f90nml.read(namelist_name)
+
+        ncfield_name = os.path.join(abspath, "snapshot.field.nc")
         if os.path.exists(ncfield_name):
             self.ncfield = self.read_ncfield(ncfield_name)
 
-        fqc_name = os.path.join(abspath, 'fqc.out')
+        fqc_name = os.path.join(abspath, "fqc.out")
         if os.path.exists(fqc_name):
             self.fqcdata = self.read_fqc(fqc_name)
-        
-        field_name = os.path.join(abspath, 'field.out')
+
+        field_name = os.path.join(abspath, "field.out")
         if os.path.exists(field_name):
             self.eigenfield = self.read_field(field_name)
 
-        map_name = os.path.join(abspath, 'map.out')
+        map_name = os.path.join(abspath, "map.out")
         if os.path.exists(map_name):
             self.fqcmapdata = self.read_fqcmap(map_name)
 
-        orbit_name = os.path.join(abspath, 'orbit.out')
+        orbit_name = os.path.join(abspath, "orbit.out")
         if os.path.exists(orbit_name):
             self.orbitdata = self.read_orbit(orbit_name)
 
@@ -55,14 +69,14 @@ class EGAMERS:
         for i1 in range(fo.neigen):
             line = f.readline()
             number_r = list(map(float, line.split()))
-            fo.omgglobal[i1] = (number_r[0])
+            fo.omgglobal[i1] = number_r[0]
             fo.gammaglobal[i1] = number_r[1]
 
         for i2 in range(fo.nr):
             line = f.readline()
             number_r = list(map(float, line.split()))
-            fo.r[i2] = (number_r[0])
-            fo.omglocal[i2] = (number_r[1])
+            fo.r[i2] = number_r[0]
+            fo.omglocal[i2] = number_r[1]
 
         for i3 in range(fo.nregam):
             line = f.readline()
@@ -91,8 +105,8 @@ class EGAMERS:
                 line = f.readline()
                 number_r = list(map(float, line.split()))
                 fo.r[i1, i2] = number_r[0]
-                fo.err[i1, i2] = (number_r[1])
-                fo.eri[i1, i2] = (number_r[2])
+                fo.err[i1, i2] = number_r[1]
+                fo.eri[i1, i2] = number_r[2]
 
         return fo
 
@@ -106,23 +120,23 @@ class EGAMERS:
         mo.icop = headerline[1]
         mo.ictp = headerline[2]
 
-        if mo.itrap == 1 :
+        if mo.itrap == 1:
             line2 = f.readline()
             trapline = list(map(int, line2.split()))
             mo.ipphin = trapline[0]
             mo.neen = trapline[1]
-            
+
             mo.pphigridn = np.zeros((mo.ipphin, mo.neen), dtype=float)
-            mo.een       = np.zeros((mo.ipphin, mo.neen), dtype=float)
-            mo.fqcn      = np.zeros((mo.ipphin, mo.neen), dtype=float)
+            mo.een = np.zeros((mo.ipphin, mo.neen), dtype=float)
+            mo.fqcn = np.zeros((mo.ipphin, mo.neen), dtype=float)
 
             for i1 in range(mo.ipphin):
                 for i2 in range(mo.neen):
                     line = f.readline()
                     data1 = list(map(float, line.split()))
                     mo.pphigridn[i1, i2] = data1[0]
-                    mo.een[i1,i2] = data1[1]
-                    mo.fqcn[i1,i2] = data1[2]
+                    mo.een[i1, i2] = data1[1]
+                    mo.fqcn[i1, i2] = data1[2]
 
         return mo
 
@@ -137,8 +151,8 @@ class EGAMERS:
         ns = list(map(int, line2.split()))
         n = ns[0]
 
-        r = np.array((0.,)*n)
-        theta = np.array((0.,)*n)
+        r = np.array((0.0,) * n)
+        theta = np.array((0.0,) * n)
 
         for i1 in range(n):
             line = f.readline()
@@ -149,7 +163,7 @@ class EGAMERS:
         x = r * np.cos(theta)
         y = r * np.sin(theta)
 
-        thetam = np.arange(0., 2*np.pi, 2*np.pi/n)
+        thetam = np.arange(0.0, 2 * np.pi, 2 * np.pi / n)
         xm = np.cos(thetam)
         ym = np.sin(thetam)
 
@@ -160,7 +174,7 @@ class EGAMERS:
         oo.ym = ym
         oo.omega = omega
         oo.n = n
-        
+
         return oo
 
     def plot_fqcmap(self, fqcmapdata=None):
@@ -169,41 +183,46 @@ class EGAMERS:
 
             plt.figure(1)
             CS = plt.contourf(
-                fqcmapdata.pphigridn.reshape(fqcmapdata.ipphin, fqcmapdata.neen), 
-                fqcmapdata.een.reshape(fqcmapdata.ipphin, fqcmapdata.neen), 
-                fqcmapdata.fqcn.reshape(fqcmapdata.ipphin, fqcmapdata.neen),cmap=plt.cm.bone
-                )
+                fqcmapdata.pphigridn.reshape(fqcmapdata.ipphin, fqcmapdata.neen),
+                fqcmapdata.een.reshape(fqcmapdata.ipphin, fqcmapdata.neen),
+                fqcmapdata.fqcn.reshape(fqcmapdata.ipphin, fqcmapdata.neen),
+                cmap=plt.cm.bone,
+            )
             cbar = plt.colorbar(CS)
-            cbar.ax.set_ylabel(r'$\omega_b$')
-            plt.title('Trapped particle frequency map')
-            plt.xlabel(r'$P_\varphi / e \Psi$')
-            plt.ylabel('E (keV)')
+            cbar.ax.set_ylabel(r"$\omega_b$")
+            plt.title("Trapped particle frequency map")
+            plt.xlabel(r"$P_\varphi / e \Psi$")
+            plt.ylabel("E (keV)")
             plt.show()
 
     def plot_fqc(self, fqcdata=None):
         if fqcdata == None:
             fqcdata = self.fqcdata
 
-        ll,=plt.plot(fqcdata.r, fqcdata.omglocal)
-        llegam,=plt.plot(fqcdata.regamlocal, fqcdata.omgegamlocal)
+        (ll,) = plt.plot(fqcdata.r, fqcdata.omglocal)
+        (llegam,) = plt.plot(fqcdata.regamlocal, fqcdata.omgegamlocal)
         for i1 in range(fqcdata.neigen):
-            gg, =plt.plot([0, 1], [fqcdata.omgglobal[i1], fqcdata.omgglobal[i1]])
+            (gg,) = plt.plot([0, 1], [fqcdata.omgglobal[i1], fqcdata.omgglobal[i1]])
         #    leg = plt.legend([gg], ['Global Mode ' + str(i1+1)])
         #    ax = plt.gca().add_artist(leg)
 
-        plt.legend([ll,llegam, gg], ['GAM continuum','EGAM continuum', 'Global EGAM'])
-        plt.ylabel(r'$Re(\Omega)$')
-        plt.xlabel('r')
-        plt.title(r'$\gamma/\omega$ = ' + str(fqcdata.gammaglobal[0]/fqcdata.omgglobal[0]))
+        plt.legend([ll, llegam, gg], ["GAM continuum", "EGAM continuum", "Global EGAM"])
+        plt.ylabel(r"$Re(\Omega)$")
+        plt.xlabel("r")
+        plt.title(
+            r"$\gamma/\omega$ = " + str(fqcdata.gammaglobal[0] / fqcdata.omgglobal[0])
+        )
 
         plt.figure(2)
-        llegam,=plt.plot(fqcdata.regamlocal, fqcdata.gammaegamlocal)
+        (llegam,) = plt.plot(fqcdata.regamlocal, fqcdata.gammaegamlocal)
         for i1 in range(fqcdata.neigen):
-            gg, =plt.plot([0, 1], [fqcdata.gammaglobal[i1], fqcdata.gammaglobal[i1]])
-        plt.legend([llegam, gg], ['EGAM continuum', 'Global EGAM'])
-        plt.ylabel(r'$\gamma=Im(\Omega)$')
-        plt.xlabel('r')
-        plt.title(r'$\gamma/\omega$ = ' + str(fqcdata.gammaglobal[0]/fqcdata.omgglobal[0]))
+            (gg,) = plt.plot([0, 1], [fqcdata.gammaglobal[i1], fqcdata.gammaglobal[i1]])
+        plt.legend([llegam, gg], ["EGAM continuum", "Global EGAM"])
+        plt.ylabel(r"$\gamma=Im(\Omega)$")
+        plt.xlabel("r")
+        plt.title(
+            r"$\gamma/\omega$ = " + str(fqcdata.gammaglobal[0] / fqcdata.omgglobal[0])
+        )
         plt.show()
 
     def plot_field(self, fielddata=None):
@@ -217,12 +236,19 @@ class EGAMERS:
 
             era = np.sqrt(np.square(err) + np.square(eri))
             plt.figure(i1)
-            rep,=plt.plot(r,err)
-            imp,=plt.plot(r,eri)
-            abp,=plt.plot(r,era)
-            plt.ylabel('Er')
-            plt.xlabel('r')
-            plt.legend([rep, imp, abp],['Real (Mode ' + str(i1+1) + ')','Imag (Mode ' + str(i1+1) + ')', 'Abs (Mode ' + str(i1+1) + ')'])
+            (rep,) = plt.plot(r, err)
+            (imp,) = plt.plot(r, eri)
+            (abp,) = plt.plot(r, era)
+            plt.ylabel("Er")
+            plt.xlabel("r")
+            plt.legend(
+                [rep, imp, abp],
+                [
+                    "Real (Mode " + str(i1 + 1) + ")",
+                    "Imag (Mode " + str(i1 + 1) + ")",
+                    "Abs (Mode " + str(i1 + 1) + ")",
+                ],
+            )
             plt.show()
 
     def plot_orbit(self, orbitdata=None):
@@ -231,6 +257,6 @@ class EGAMERS:
 
         plt.figure(1)
         plt.plot(orbitdata.x, orbitdata.y, orbitdata.xm, orbitdata.ym)
-        plt.axis('equal')
-        plt.title(r'$\omega_b$ = ' + str(orbitdata.omega))
+        plt.axis("equal")
+        plt.title(r"$\omega_b$ = " + str(orbitdata.omega))
         plt.show()
--- py/pyegamers/ncfield.py
+++ py/pyegamers/ncfield.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read_ncfield(self, ncfield_name):
     fo = self.OutputData()
 
@@ -11,23 +12,23 @@ def read_ncfield(self, ncfield_name):
 
     for name in rootgrp.variables:
         if name in keyword.kwlist:
-            name1 = name + '_'
+            name1 = name + "_"
         else:
             name1 = name
         setattr(fo, name1, np.array(rootgrp.variables[name][:]))
-    
+
     re = rootgrp["/radial_element"]
-    radial_attributes = ['lfieldoutput', 'igrid_type', 'XR1', 'XR2', 'SIG1', 'SIG2']
+    radial_attributes = ["lfieldoutput", "igrid_type", "XR1", "XR2", "SIG1", "SIG2"]
     for p in radial_attributes:
         setattr(fo, p, getattr(re, p))
 
     rootgrp.close()
 
     nele = fo.radial_element.size
-    H1_elements = np.arange(1, nele-1, 2)
-    H2_elements = np.concatenate([np.arange(0,nele,2), [nele-1]])
+    H1_elements = np.arange(1, nele - 1, 2)
+    H2_elements = np.concatenate([np.arange(0, nele, 2), [nele - 1]])
     fo.r = fo.radial_element[H2_elements]
-    
+
     nax0 = np.zeros([fo.time.size, 1])
 
     fo.eta_H1 = np.concatenate([nax0, fo.eta[:, H1_elements], nax0], 1)
@@ -38,72 +39,139 @@ def read_ncfield(self, ncfield_name):
 
     return fo
 
-def compute_ncfield(self, r=np.linspace(0,1,101), tslice=[0], ncfield=None):
+
+def compute_ncfield(self, r=np.linspace(0, 1, 101), tslice=[0], ncfield=None):
     from scipy.interpolate import CubicHermiteSpline
-    if ncfield==None:
+
+    if ncfield == None:
         ncfield = self.ncfield
 
     tslice = np.atleast_1d(tslice)
     t = ncfield.time[tslice]
 
-    lambda_CH = CubicHermiteSpline(ncfield.r, ncfield.lambda_H1[tslice,:], ncfield.lambda_H2[tslice,:], axis=1)
-    eta_CH = CubicHermiteSpline(ncfield.r, ncfield.eta_H1[tslice,:], ncfield.eta_H2[tslice,:], axis=1)
+    lambda_CH = CubicHermiteSpline(
+        ncfield.r, ncfield.lambda_H1[tslice, :], ncfield.lambda_H2[tslice, :], axis=1
+    )
+    eta_CH = CubicHermiteSpline(
+        ncfield.r, ncfield.eta_H1[tslice, :], ncfield.eta_H2[tslice, :], axis=1
+    )
 
     return t, r, lambda_CH(r), eta_CH(r)
 
-def plot_field_radial(self, r=np.linspace(0,1,101), tslice=[0], variable='lambda', ncfield=None):
+
+def compute_gamma(
+    self, tslice_start=0, tslice_end="last", variable="lambda", ncfield=None
+):
+
+    from scipy.optimize import curve_fit
+
+    if ncfield == None:
+        ncfield = self.ncfield
+
+    if tslice_end == "last":
+        tslice_end = ncfield.time.size - 1
+
+    tslice = np.arange(tslice_start, tslice_end + 1)
+    t = ncfield.time[tslice]
+    eng = ncfield.field_energy[tslice]
+    lneng = np.log(eng)
+
+    popt, pcov = curve_fit(lambda x, a, b : a * x + b, t, lneng )
+
+    return popt[0] / 2
+
+def plot_field_energy(
+    self, tslice_start=0, tslice_end="last", variable="lambda", log=True, ncfield=None
+):
+
+    if ncfield == None:
+        ncfield = self.ncfield
+
+    if tslice_end == "last":
+        tslice_end = ncfield.time.size - 1
+
+    tslice = np.arange(tslice_start, tslice_end + 1)
+    t = ncfield.time[tslice]
+    eng = ncfield.field_energy[tslice]
+
+    if log:
+        plt.semilogy(t, eng)
+    else:
+        plt.plot(t, eng)
+    plt.xlabel('t')
+    plt.ylabel('energy')
+
+
+def plot_field_radial(
+    self, r=np.linspace(0, 1, 101), tslice=[0], variable="lambda", ncfield=None
+):
 
     tslice = np.atleast_1d(tslice)
     t, r, lambda_, eta = self.compute_ncfield(r=r, tslice=tslice, ncfield=ncfield)
     nt = tslice.size
 
     for i in range(nt):
-        if variable == 'lambda':
-            plt.plot(r, lambda_[i,:])
+        if variable == "lambda":
+            plt.plot(r, lambda_[i, :])
         else:
-            plt.plot(r, eta[i,:])
-        
-    plt.legend(['t=' + "{0:.4g}".format(t1) for t1 in t ])
+            plt.plot(r, eta[i, :])
 
-def plot_field_time(self, r=[0.5], tslice_start=0, tslice_end='last', variable='lambda', ncfield=None):
+    plt.legend(["t=" + "{0:.4g}".format(t1) for t1 in t])
+
+
+def plot_field_time(
+    self, r=[0.5], tslice_start=0, tslice_end="last", variable="lambda", ncfield=None
+):
     r = np.atleast_1d(r)
     nr = r.size
-    if ncfield==None:
+    if ncfield == None:
         ncfield = self.ncfield
 
-    if tslice_end == 'last':
+    if tslice_end == "last":
         tslice_end = ncfield.time.size - 1
 
-    t, r, lambda_, eta = self.compute_ncfield(r=r, tslice=np.arange(tslice_start, tslice_end), ncfield=ncfield)
+    tslice = np.arange(tslice_start, tslice_end + 1)
+
+    t, r, lambda_, eta = self.compute_ncfield(r=r, tslice=tslice, ncfield=ncfield)
 
     for i in range(nr):
-        if variable == 'lambda':
-            plt.plot(t, lambda_[:,i])
+        if variable == "lambda":
+            plt.plot(t, lambda_[:, i])
         else:
-            plt.plot(t, eta[:,i])
-        
-    plt.legend(['r=' + "{0:.4g}".format(r1) for r1 in r ])
-    
+            plt.plot(t, eta[:, i])
 
-def plot_spectrogram(self, r=0.5, tslice_start=0, tslice_end='last', variable='lambda', ncfield=None, **kwargs):
+    plt.legend(["r=" + "{0:.4g}".format(r1) for r1 in r])
+
+
+def plot_spectrogram(
+    self,
+    r=0.5,
+    tslice_start=0,
+    tslice_end="last",
+    variable="lambda",
+    ncfield=None,
+    **kwargs
+):
 
     r = np.atleast_1d(r)
 
-    if ncfield==None:
+    if ncfield == None:
         ncfield = self.ncfield
 
-    if tslice_end == 'last':
+    if tslice_end == "last":
         tslice_end = ncfield.time.size - 1
 
-    t, r, lambda_, eta = self.compute_ncfield(r=r, tslice=np.arange(tslice_start, tslice_end), ncfield=ncfield)
+    tslice = np.arange(tslice_start, tslice_end + 1)
+
+    t, r, lambda_, eta = self.compute_ncfield(r=r, tslice=tslice, ncfield=ncfield)
 
     from scipy import signal
 
-    fs = (t.size - 1) / (t[-1] - t[0]) 
+    fs = (t.size - 1) / (t[-1] - t[0])
 
-    if variable == 'lambda':
-        data = lambda_[:,0]
+    if variable == "lambda":
+        data = lambda_[:, 0]
     else:
-        data =  eta[:,0]
+        data = eta[:, 0]
 
     plt.specgram(data, Fs=fs, **kwargs)
